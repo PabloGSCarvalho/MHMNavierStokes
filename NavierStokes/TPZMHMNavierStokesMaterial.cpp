06dbd4c4eb08eb3bd5b85e5ef6b2d116f535ca11
PabloGSCarvalho <pablogscarvalho@hotmail.com>
2020-09-09 21:40:00 -0300

    Obstacle test 008 - Transient - NS - No-Slip condition in all - Linear Cte

--- NavierStokes/TPZMHMNavierStokesMaterial.cpp
+++ NavierStokes/TPZMHMNavierStokesMaterial.cpp
@@ -196,8 +196,14 @@ void TPZMHMNavierStokesMaterial::ContributeBC(TPZVec<TPZMaterialData> &datavec, 
 
         REAL time = f_sim_data->GetTime();
 
-        v_Dirichlet[0] = v_Dirichlet[0]*(sin(M_PI*(time+0.1-0.5))*0.5+0.5);
-        v_Dirichlet[1] = v_Dirichlet[1]*(sin(M_PI*(time+0.1-0.5))*0.5+0.5);
+
+        //v_Dirichlet[0] = v_Dirichlet[0]*(sin(M_PI*(time+0.1-0.5))*0.5+0.5);
+        //v_Dirichlet[1] = v_Dirichlet[1]*(sin(M_PI*(time+0.1-0.5))*0.5+0.5);
+
+        if(time<0.9){
+            v_Dirichlet[0] = v_Dirichlet[0]*(time+0.1);
+            v_Dirichlet[1] = v_Dirichlet[1]*(time+0.1);
+        }
 
 
         // Calculo do vetor (Du)n :
